ac19158af7bd0a0e98a407f68c6615d7b49dd27a
Joel Abshier <JAbshier@midwayusa.com>
2021-01-27 16:51:49 -0600

    Added create-elm-app

---
+++ src/Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, a, button, div, h1, img, span, text)
+import Html.Attributes exposing (class, src)
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { items : List String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { items = [ "Do Thing", "Make Work Happen", "Execute Action" ] }
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "main" ]
+        [ h1 [] [ text "Two Dew Elm" ]
+        , div [ class "todo-list" ] (List.map viewItem model.items)
+        ]
+
+
+viewItem : String -> Html Msg
+viewItem item =
+    div [ class "list-item" ]
+        [ span [ class "list-item-text" ] [ text item ]
+        , span [ class "list-item-actions" ]
+            [ button [ class "icon-button" ] [] ]
+        ]
