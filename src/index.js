6588b7f3d25d459efd6bf1dfd1954ef29f145038
Joel Abshier <JAbshier@midwayusa.com>
2021-02-08 15:06:04 -0600

    Remove `serverworker` from create elm app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
-
 import './css/main.scss';
 
 const app = Elm.Main.init({
   node: document.getElementById('root'),
   flags: 'Joel',
 });
-
-serviceWorker.unregister();
